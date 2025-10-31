d05e7f014b298cc706ae988a477ba9b8200c16e3
Payden Hoskins <paydenhoskins@gmail.com>
2025-10-30 19:50:46 -0600

    More minor changes.

--- FirstProject2/FirstProject2.X/nbproject/Makefile-variables.mk
+++ FirstProject2/FirstProject2.X/nbproject/Makefile-variables.mk
@@ -6,8 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=FirstProject2.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/FirstProject2.X.production.hex
-CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=firstproject2.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/firstproject2.x.tar
+CND_ARTIFACT_NAME_default=FirstProject2.X.production.null
+CND_ARTIFACT_PATH_default=dist/default/production/FirstProject2.X.production.null
